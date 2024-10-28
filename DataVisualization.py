d0f9d0e0be9dceb8d5b75d5fd52f37bc6cfc5ed9
Sena Sır <sirsena10@gmail.com>
2024-10-28 23:56:54 +0300

    Data Visualization

---
+++ DataVisualization.py
@@ -0,0 +1,31 @@
+# DATA VISUALİZATION: MATPLOTLIB & SEABORN
+
+# MATPLOTLIB:
+
+# Categorical Variable: column chart. countplot (seaborn), bar(matplotlib)
+# Numerical Variable: hist, boxplot
+
+# CATEGORICAL VARIABLE VISUALIZATION:
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["sex"].value_counts().plot(kind='bar')
+plt.show(block=True)
+
+
+
+
+
+
+
+
+
+
+
+
