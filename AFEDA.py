d6226e13a608a828d9c3700ef65cb436b12f6f22
Sena Sır <sirsena10@gmail.com>
2024-11-02 23:52:02 +0300

    Advanced Functional Exploratory Data Analysis

---
+++ AFEDA.py
@@ -0,0 +1,38 @@
+# OVERVİEW
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+df.shape
+df.info()
+df.columns
+df.index
+df.describe().T
+df.isnull().values.any()
+df.isnull().sum()
+
+# Gives an overview of the data:
+def check_df(dataframe, head=5):
+       print("#######SHAPE############")
+       print(dataframe.shape)
+       print("#######TYPES############")
+       print(dataframe.dtypes)
+       print("#######HEAD############")
+       print(dataframe.head(head))
+       print("#######NA############")
+       print(dataframe.isnull().sum())
+       print("#######TAIL############")
+       print(dataframe.tail(head))
+       print("#######QUANTILES############")
+       print(dataframe.describe([0, 0.05, 0.50, 0.95, 1]).T)
+
+
+check_df(df)
+
+df = sns.load_dataset("flights")
+check_df(df)
