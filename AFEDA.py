253370434640ae6e5ec8ddc56da76e72397b6d90
Sena Sır <sirsena10@gmail.com>
2024-12-08 19:43:33 +0300

    categorical variable analysis
    categorical variable analysis using function and list comprehension.

--- AFEDA.py
+++ AFEDA.py
@@ -32,19 +32,75 @@ def check_df(dataframe, head=5):
        print(dataframe.describe([0, 0.05, 0.50, 0.95, 1]).T)
 
 
-check_df(df)
 
-df = sns.load_dataset("flights")
 check_df(df)
+print(df.columns)
+# CATEGORICAL VARIABLE ANALYSIS:
 
-# CATEGORICAL VARIABLE ANALYSIS
 df["embarked"].value_counts()
 df["sex"].unique()
-df["sex"].nunique()
-df["embarked"].nunique()
-df.info()
+df["class"].nunique()
+df["survived"].value_counts()  # actually a categorical variable.
 
-cat_cols = [col for col in df.columns if str(df[col].dtypes) in ["category", "object", "bool"]]
+# catching the variables depends on it's datatype.
+cat_cols = [col for col in df.columns if str(df[col].dtypes) in ["category", "object", "bool"] ]
+print(cat_cols)
+# str(df["sex"].dtypes)in ["object"]  # True
+
+# catching the variables that are actually categorical but seems numerical:
+# catch if the uniqe class number less than 10:
+num_but_cat = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ["int", "float"]]
+print(num_but_cat)
+
+# catching the variables that are actually numerical but seems categorical:
+# there are too many classes to define.
+# high cardinal categorical columns.
+cat_but_car = [col for col in df.columns if df[col].nunique() > 20 and str(df[col].dtypes) in ["category", "object"]]
+print(cat_but_car)  # there is none.
+
+# all the categorical variables in the same list:
+cat_cols = cat_but_car + num_but_cat
+
+# deleting the high kardinal variables.
+cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+df[cat_cols]
+
+df[cat_cols].nunique()
+
+# variables that not in cat_cols :
+[col for col in df.columns if col not in cat_cols]
+
+######
+
+df["survived"].value_counts() # frequency info.
+100 * df["survived"].value_counts() / len(df) # percentage information of the revelant dataframe.
+
+# it calculates the frequencies and percentage ratios of the values in a categorical column,
+# and presents them in an easily analyzable table format:
+def cat_summary(dataframe, col_name):
+       print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                           "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+       print("##############################")
+
+cat_summary(df, "sex")
+
+# all the categorical variabels on dataset showed categorically.
+for col in cat_cols:
+       cat_summary(df, col)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
